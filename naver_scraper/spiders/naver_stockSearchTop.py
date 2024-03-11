5026002cdc8288b8ebda84b8ad1a98dc3f720a16
KimJeaCheol <cresh0105@gmila.com>
2024-03-11 23:57:38 +0900

    인기종목 검색 완성

--- naver_scraper/spiders/naver_stockSearchTop.py
+++ naver_scraper/spiders/naver_stockSearchTop.py
@@ -8,21 +8,22 @@ class NaverFinanceStockSearchTop(scrapy.Spider):
 
     def parse(self, response):
         # article2 밑에 있는 section1과 section2를 구분합니다.
-        content_area = response.css('#contentarea')
-        rows = content_area.css('div.box_type_l')
-        test = rows.css('table')
-        print(test)
-        
+        #content_area = response.css('div.box_type_l table').extract()
+        #rows = response.xpath('//*[@id="contentarea"]/div[3]/table/tr')
+        rows = response.css('#contentarea > div.box_type_l > table > tr')
         # 각 section에 대해 반복합니다.
         for row in rows[1:]:  # 첫 번째 행은 테이블의 헤더이므로 건너뜁니다
+            if len(row.css('td')) == 1 and not row.css('td.blank'):
+                continue  # 빈 데이터는 건너뜁니다.
+            
             item = stockSearchTop()
-            # 각 행의 데이터를 추출합니다.
+            # 각 행의 데이터를 추출합니다
             item['rank_num'] = row.css('td:nth-child(1)::text').get()
             item['stock_name'] = row.css('td:nth-child(2) a::text').get()
             item['search_rate'] = row.css('td:nth-child(3)::text').get()
             item['current_price'] = row.css('td:nth-child(4)::text').get()
-            item['change_value'] = row.css('td:nth-child(5)::text').get()
-            item['change_percent'] = row.css('td:nth-child(6)::text').get()
+            item['change_value'] = row.css('td:nth-child(5) span::text').get().strip()
+            item['change_percent'] = row.css('td:nth-child(6) span::text').get().strip()
             item['volume'] = row.css('td:nth-child(7)::text').get()
             item['opening_price'] = row.css('td:nth-child(8)::text').get()
             item['high_price'] = row.css('td:nth-child(9)::text').get()
